a7af38a4613ee5ddb5d14279fc4680d18150a21b
dhanushka365 <technohub10000@gmail.com>
2022-06-12 14:27:44 +0800

    works 1

---
+++ src/main/java/tech/getarrays/employeemanager/monitoring/DatabaseService.java
@@ -0,0 +1,23 @@
+package tech.getarrays.employeemanager.monitoring;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseService implements HealthIndicator {
+    private final String DATABASE_SERVICE = "Database Service";
+    @Override
+    public Health health() {
+        if(isDatabaseServiceGood()){
+            return Health.up().withDetail(DATABASE_SERVICE, "Service is running").build();
+
+        }
+        return Health.down().withDetail(DATABASE_SERVICE, "Service is not available").build();
+    }
+
+    private boolean isDatabaseServiceGood(){
+        //logic
+        return true;
+    }
+}
